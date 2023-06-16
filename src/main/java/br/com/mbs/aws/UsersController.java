43c353ac0f1c1370c91180cb78b8bca7bae383ad
Marcelo Barbosa Soares <treinamento@rodosoft.com.br>
2023-06-16 15:00:28 -0300

    upgrade project

---
+++ src/main/java/br/com/mbs/aws/UsersController.java
@@ -0,0 +1,31 @@
+package br.com.mbs.aws;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequestMapping("users")
+@RestController("users")
+public class UsersController {
+
+    private List<User> listUser = new ArrayList<>();
+    private static int contId;
+    @GetMapping
+    public ResponseEntity<List<User>> getUser(){
+        return ResponseEntity
+                .ok()
+                .body(listUser);
+    }
+
+    @PostMapping
+    public ResponseEntity<Integer> createUser(@RequestBody User user){
+        user.setId(++contId);
+        listUser.add(user);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(contId);
+    }
+}
